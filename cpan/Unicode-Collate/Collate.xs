stevan/perl

XXX CPAN prob wrong Collate
This changes to implicity usenative code points.  This is likely wrong,
as the module comes with its own data, that are probably in terms of
Unicode

cpan/Unicode-Collate/Collate.xs
@@ -10,10 +10,18 @@
 /* This file is prepared by mkheader */
 #include "ucatbl.h"
 
-/* Perl 5.6.1 ? */
-#ifndef utf8n_to_uvuni
-#define utf8n_to_uvuni  utf8_to_uv
-#endif /* utf8n_to_uvuni */
+#ifdef uvoffuni_to_utf8
+    /* On modern Perls, the translation tables are stored in native order, so
+     * we want uvchr() instead of uvuni() */
+#   undef utf8n_to_uvuni
+#   define utf8n_to_uvuni   utf8n_to_uvchr
+#else
+
+    /* Perl 5.6.1 ? */
+#   ifndef utf8n_to_uvuni
+#       define utf8n_to_uvuni   utf8_to_uv
+#   endif /* utf8n_to_uvuni */
+#endif
 
 /* UTF8_ALLOW_BOM is used before Perl 5.8.0 */
 #ifndef UTF8_ALLOW_BOM

xatier/perl5

cpan/Unicode-Collate/Collate.xs
@@ -10,10 +10,18 @@
 /* This file is prepared by mkheader */
 #include "ucatbl.h"
 
-/* Perl 5.6.1 ? */
-#ifndef utf8n_to_uvuni
-#define utf8n_to_uvuni  utf8_to_uv
-#endif /* utf8n_to_uvuni */
+#ifdef NATIVE_SKIP
+    /* On modern Perls, the translation tables are stored in native order, so
+     * we want uvchr() instead of uvuni() */
+#   undef utf8n_to_uvuni
+#   define utf8n_to_uvuni   utf8n_to_uvchr
+#else
+
+    /* Perl 5.6.1 ? */
+#   ifndef utf8n_to_uvuni
+#       define utf8n_to_uvuni   utf8_to_uv
+#   endif /* utf8n_to_uvuni */
+#endif
 
 /* UTF8_ALLOW_BOM is used before Perl 5.8.0 */
 #ifndef UTF8_ALLOW_BOM